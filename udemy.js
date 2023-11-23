50f2a0ea70086cb01569363b1f56851f3ae8a222
Nicholas Reyaad Rafeek <45085276+Reyaad510@users.noreply.github.com>
2023-11-23 15:38:40 -0500

    Calc BMI

--- udemy.js
+++ udemy.js
@@ -1,6 +1,7 @@
 
 // Make first letter of a string capitalized
 
+/*
 let name = "reyaAD";
 
 let firstChar = name.slice(0,1).toUpperCase();
@@ -10,7 +11,34 @@ let restOfName = name.slice(1, name.length).toLowerCase();
 let fullName = firstChar + restOfName;
 
 
-
 console.log(firstChar);
 console.log(restOfName)
 console.log(`I am ${fullName}`);
+*/
+
+
+
+// BMI
+
+
+/*
+    BMI = weight(kg)/ height^2(m^2)
+
+*/
+
+
+
+const bmiCalculator = (weight, height) => {
+    let bmi = 0;
+
+    return bmi = Math.floor(weight/Math.pow(height,2));
+
+}
+
+
+console.log(bmiCalculator(65,1.8));
+
+
+
+
+
